766f58aca160af2cef58437bb68740eb444faac6
Marco Lamina <mlamina09@gmail.com>
2024-06-09 18:15:06 -0700

    Fix cryptography module

--- engine/cryptography.py
+++ engine/cryptography.py
@@ -2,16 +2,19 @@ import base64
 import hashlib
 
 from cryptography.fernet import Fernet
-
 from django.conf import settings
 
-key = base64.urlsafe_b64encode(hashlib.sha256(settings.SECRET_KEY.encode()).digest()[:32])
-fernet = Fernet(key)
+
+def load_fernet():
+    key = base64.urlsafe_b64encode(hashlib.sha256(settings.SECRET_KEY.encode()).digest()[:32])
+    return Fernet(key)
 
 
 def encrypt(value):
+    fernet = load_fernet()
     return fernet.encrypt(value.encode()).decode()
 
 
 def decrypt(value):
+    fernet = load_fernet()
     return fernet.decrypt(value.encode()).decode()
